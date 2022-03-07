ff7a7b7f786f906dee30e88a8fabbbf73ccdf13b
yawar2019 <yawarali17@gmail.com>
2022-03-07 21:59:03 +0530

    Committed

--- TeamConsoleTigers/SwitchCaseExample.cs
+++ TeamConsoleTigers/SwitchCaseExample.cs
@@ -39,4 +39,138 @@ namespace TeamConsoleTigers
         }
 
     }
+
+    class SwitchCaseExample2
+    {
+        public static void Main()
+        {
+           
+            Console.WriteLine("Please select 1)cloths\n 2)watch");
+            char Choice = Convert.ToChar(Console.ReadLine());
+            switch (Choice)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    Console.WriteLine("Vowels is Entered");
+                    break;
+                
+
+                default:
+                    Console.WriteLine("Consonent Entered");
+                    break;
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+    class SwitchCaseExample3
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("Please select 1)cloths\n 2)watch");
+            int Choice = Convert.ToInt32(Console.ReadLine());
+            switch (Choice)
+            {
+                case 1:
+                    Console.WriteLine("Please select 1)shirt\n2)jeans\n3)shoes");
+                    int Choice2 = Convert.ToInt32(Console.ReadLine());
+
+                    switch(Choice2)
+                    {
+                        case 1:
+                            Console.WriteLine("shirt of 2000 selected");
+                            break;
+                        case 2:
+                            Console.WriteLine("jeans of 2000 selected");
+
+                            break;
+                        case 3:
+                            Console.WriteLine("shoes of 2000 selected");
+
+                            break;
+                        default:
+                            Console.WriteLine("Np item is selected");
+                            break;
+                    }
+
+                    break;
+                case 2:
+                    Console.WriteLine("Please select 1)armani\n2)titan\n3)clock");
+                    int Choice3= Convert.ToInt32(Console.ReadLine());
+
+                    switch (Choice3)
+                    {
+                        case 1:
+                            Console.WriteLine("armani watch of 2000 selected");
+                            break;
+                        case 2:
+                            Console.WriteLine("titan watch of 2000 selected");
+
+                            break;
+                        case 3:
+                            Console.WriteLine("clock watch of 2000 selected");
+
+                            break;
+                        default:
+                            Console.WriteLine("No watch selected item is selected");
+                            break;
+                    }
+
+                    break;
+
+
+                default:
+                    Console.WriteLine("no item is selected");
+                    break;
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+    class SwitchCaseExample4
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("Enter a value");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b value");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Press + for Add\n- for Sub\n* for Mul\n \\ for div\n ");
+            char Choice = Convert.ToChar(Console.ReadLine());
+            switch (Choice)
+            {
+                case '+':
+                    Console.WriteLine(a+b);
+                    break;
+                case '-':
+                    Console.WriteLine(a - b);
+                    break;
+                case '*':
+                    Console.WriteLine(a * b);
+                    break;
+                case '/':
+                    Console.WriteLine(a / b);
+                    break;
+               
+                default:
+                    Console.WriteLine("no Options is selected");
+                    break;
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
 }
