bce6fd6949f72dd5a4b86f5d2cbaa6455c182ff6
yawar2019 <yawarali17@gmail.com>
2022-03-19 22:18:54 +0530

    constructor

--- TeamConsoleTigers/MethodOverridingExample.cs
+++ TeamConsoleTigers/MethodOverridingExample.cs
@@ -9,12 +9,12 @@ namespace TeamConsoleTigers
     class MethodOverridingExample
     {
         public int a;
-        public virtual void Display()
+        public  void Display()
         {
             Console.WriteLine("parent class display method is called");
         }
 
-        public void Display2()
+        public  void Display2()
         {
             Console.WriteLine("parent class display2 method is called");
         }
@@ -22,7 +22,7 @@ namespace TeamConsoleTigers
     }
     class MethodOverridingExample2: MethodOverridingExample
     {
-        public override void Display()
+        public new void Display()
         {
             Console.WriteLine("Child class display method is called");
         }
@@ -32,12 +32,28 @@ namespace TeamConsoleTigers
         }
         public static void Main()
         {
-            MethodOverridingExample2 obj = new MethodOverridingExample2();
-            obj.Display();
-            obj.Display2();
+            MethodOverridingExample m1 = new MethodOverridingExample2();
+            m1.Display();
+            m1.Display2();
+            MethodOverridingExample2 m2 = new MethodOverridingExample2();
+            m2.Display();
+            m2.Display2();
             Console.ReadLine();
         }
     }
 }
 //To Implement Method Overriding Parent Class must need to have Virtual keyword which will give child class
 //to reimplement method inside child class .and Child class will override by using overrid keyword
+//If ur parent class method is virtual and child class is overriding your parent class method 
+//and creating parent class variable and pointing parent class variable to child class memory then it will called
+//child class methods
+//2)if ur creating parent class variable and pointing to child class variable then 
+// it will called parent class method if your doing method hiding or your child class method is
+//having new keyword before method name.
+//methodoverriding is also called runtime polymorphism
+//method overloading can be called compile time polymorphism.
+
+//what is early binding method and late binding method?
+//Method overloading orCompile time bidning  is Early binding method
+//Method overridingis Late Binding method. 
+
---
+++ TeamConsoleTigers/inheritance1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamConsoleTigers
+{
+    class inheritance1
+    {
+       public  inheritance1()
+        {
+            Console.WriteLine("Inheritance1 constructor is called");
+
+        }
+        public void display() {
+            Console.WriteLine("Inheritance1 is called");
+        }
+
+    }
+    class inheritance2: inheritance1
+    {
+        public  inheritance2()
+        {
+            Console.WriteLine("Inheritance2 constructor is called");
+
+        }
+        public void display()
+        {
+            base.display();
+            Console.WriteLine("Inheritance2 is called");
+        }
+    }
+    class inheritance3: inheritance2
+    {
+        public  inheritance3()
+        {
+            Console.WriteLine("Inheritance3 constructor is called");
+
+        }
+        public  void display()
+        {
+            Console.WriteLine("Inheritance3 is called");
+        }
+    }
+    class inheritance4
+    {
+        public  inheritance4()
+        {
+            Console.WriteLine("Inheritance4 constructor is called");
+
+        }
+        public static void Main()
+        {
+            inheritance2 obj = new inheritance3();
+            obj.display();
+            Console.ReadLine();
+        }
+    }
+}
